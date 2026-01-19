410c6a5e35747382151466f813c366eaba1c2ba7
harshgohel46 <harshgohel46@gmail.com>
2026-01-19 00:42:20 -0700

    feat: implemented the eureka discovery server

---
+++ Backend/Discovery-server/src/main/java/backend/discovery/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package backend.discovery.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; // <--- CHECK THIS LINE
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+}
---
+++ Backend/Discovery-server/src/test/java/backend/discovery/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.discovery.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
